67531fe9664de6115d01817390939f896d40e0f5
vladi642 <Vladisto05@gmail.com>
2023-05-17 22:52:18 +0300

    Добавих класовете (Food / Drink) който наследяват/използват класа (Product)  и също така съм добавил и Exception-и .

---
+++ store system/store system/Drink.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace store_system
+{
+    internal class Drink : Product
+    {
+        public Drink(string name, int quantity, double deliverPrice , double percentageMakeup) : base(name , deliverPrice, percentageMakeup)
+        {
+            if (percentageMakeup > 200)
+            {
+                throw new ArgumentException("Drink percentage cannot be more than 200%!");
+            }
+        }
+    }
+}
---
+++ store system/store system/Food.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace store_system
+{
+    public class Food : Product
+    {
+        public virtual double percentageMarkup
+        {
+            get { return base.PercentageMarkup; }
+            set
+            {
+                if (value > 100)
+                {
+                    throw new ArgumentException("Food percentage cannot be more than 100%!");
+                }
+                base.PercentageMarkup = value;
+            }
+        }
+        public Food(string name, int quantity, double deliverPrice, double percentageMarkup) : base(name, quantity, deliverPrice, percentageMarkup)
+        {
+
+        }
+        
+    }
+}
